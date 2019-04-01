389a3ef9a151b985e1b28b4216f5a7c7bdec2097
ikeda3642 <makiikeda3642@gmail.com>
2019-04-01 23:03:31 +0900

    master : change master temp

--- PracticeConsoleApp/Program.cs
+++ PracticeConsoleApp/Program.cs
@@ -6,7 +6,14 @@ namespace PracticeConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("test");
+            Console.WriteLine("please M");
+            string input = Console.ReadLine();
+            int M;
+            if(!int.TryParse(input.Split(' ')[0], out M)) return;
+            //int N;
+            //if(!int.TryParse(input.Split(' ')[1], out N)) return;
+            Console.WriteLine("Output is");
+            Console.WriteLine(M.ToString());
         }
     }
 }
