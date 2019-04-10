697d785299e1782d7d1c3b358dc4ae71cd198e35
ikeda3642 <makiikeda3642@gmail.com>
2019-04-10 23:25:59 +0900

    P_D114 : complete

--- PracticeConsoleApp/Program.cs
+++ PracticeConsoleApp/Program.cs
@@ -1,22 +1,24 @@
 ﻿using System;
 
-namespace P_D115
+namespace P_D114
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //Console.WriteLine("please a b");
+            string input_ab = Console.ReadLine();
             
-            //Console.WriteLine("please N");
-            string input_N = Console.ReadLine();
-
-            int N;
-            if(!int.TryParse(input_N, out N)) return;
-            if(N < 2 || N > 50) return;
+            double a;
+            if(!double.TryParse(input_ab.Split(' ')[0], out a)) return;
+            if(a < 0D || a > 50D) return;
             
-            int result = N%2==0 ? N/2 : (N-1)/2;
+            double b;
+            if(!double.TryParse(input_ab.Split(' ')[1], out b)) return;
+            if(b < 1D || b > 10000D) return;
+            double result = Math.Floor(b + b*a/100D);
 
-            Console.WriteLine(result.ToString());
+            Console.WriteLine(Convert.ToInt32(result).ToString());
         }
     }
 }
