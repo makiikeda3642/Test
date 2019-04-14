c86c67df6a5df12a978c54a9c4d2e0daffa6e77e
ikeda3642 <makiikeda3642@gmail.com>
2019-04-14 21:44:26 +0900

    P_D097 : not complete

--- PracticeConsoleApp/Program.cs
+++ PracticeConsoleApp/Program.cs
@@ -4,26 +4,23 @@ namespace P_D097
 {
     class Program
     {
+        public static int CountChar(string s, char c) {
+            return s.Length - s.Replace(c.ToString(), "").Length;
+        }
         static void Main(string[] args)
         {
-            //Console.WriteLine("please input");
+            Console.WriteLine("please input");
             string input_line_1 = Console.ReadLine();
 
-            int counter = 0;
+            int have_I = CountChar(input_line_1,'I') > 0 ? 1:0;
+            int have_l = CountChar(input_line_1,'l') > 0 ? 1:0;
+            int have_i = CountChar(input_line_1,'i') > 0 ? 1:0;
 
-            for(int i=0; i<7; i++)
-            {
-                int N;
-                if(!int.TryParse(input_line_1.Split(' ')[i] , out N)) return;
-                
-                counter+=N;
-            }
-            
-            string result = "no";
+            string result = input_line_1;
 
-            if(counter >= 5)
+            if((have_I + have_l + have_i) >= 2)
             {
-                result = "yes";
+                result = "caution";
             }
 
             Console.WriteLine(result);
