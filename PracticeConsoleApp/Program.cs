0cdd21fb92637ebf11b4ddbe3cbf283dbe4e3b95
ikeda3642 <makiikeda3642@gmail.com>
2019-04-14 20:36:25 +0900

    P_D105 : complete

--- PracticeConsoleApp/Program.cs
+++ PracticeConsoleApp/Program.cs
@@ -1,27 +1,21 @@
 ﻿using System;
 
-namespace P_D106
+namespace P_D105
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("please input M");
+            //Console.WriteLine("please input S");
             string input_line_1 = Console.ReadLine();
-
-            int N;
-            if(!int.TryParse(input_line_1 , out N)) return;
-            if(N < 1 || N > 100) return;
-
-            //Console.WriteLine("please input N");
             string input_line_2 = Console.ReadLine();
-            int M;
-            if(!int.TryParse(input_line_2 , out M)) return;
-            if(M < 1 || M > 100) return;
 
-            int result = N%M;
-
-            Console.WriteLine(result.ToString());
+            string result = "No";
+            if(input_line_1.Length == input_line_2.Length)
+            {
+                result = "Yes";
+            }
+            Console.WriteLine(result);
         }
     }
 }
