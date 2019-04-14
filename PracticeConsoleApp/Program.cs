2da8c7685d3f3f4fc2022c529d5b55b2824889ac
ikeda3642 <makiikeda3642@gmail.com>
2019-04-14 19:52:21 +0900

    P_D110 : complete

--- PracticeConsoleApp/Program.cs
+++ PracticeConsoleApp/Program.cs
@@ -1,30 +1,27 @@
 ﻿using System;
 
-namespace P_D111
+namespace P_D110
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("please n");
-            string input_n = Console.ReadLine();
+            //Console.WriteLine("please abc");
+            string input_abc = Console.ReadLine();
             
-            int n;
-            if(!int.TryParse(input_n , out n)) return;
-            if(n < 1 || n > 100) return;
+            int a;
+            if(!int.TryParse(input_abc.Split(' ')[0] , out a)) return;
+            if(a < 0 || a > 100) return;
             
-            //Console.WriteLine("please n");
-            string input_S = Console.ReadLine();
+            int b;
+            if(!int.TryParse(input_abc.Split(' ')[1] , out b)) return;
+            if(b < 0 || b > 100) return;
 
-            string result = "";
-            if(input_S.Length >= n)
-            {
-                result = input_S.Substring(0,n);
-            }
-            else
-            {
-                result = input_S;
-            }
+            int c;
+            if(!int.TryParse(input_abc.Split(' ')[2] , out c)) return;
+            if(c < 0 || c > 100) return;
+
+            int result = a*b*c;
 
             Console.WriteLine(result.ToString());
         }
