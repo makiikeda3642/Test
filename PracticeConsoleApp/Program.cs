f6945ca3106cae08ad0a8b09d4fa8a6755381a46
ikeda3642 <makiikeda3642@gmail.com>
2019-04-14 21:22:07 +0900

    P_D098 : comp

--- PracticeConsoleApp/Program.cs
+++ PracticeConsoleApp/Program.cs
@@ -1,19 +1,25 @@
 ﻿using System;
 
-namespace P_D099
+namespace P_D098
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("please input S");
+            //Console.WriteLine("please input a b");
             string input_line_1 = Console.ReadLine();
+            int a;
+            if(!int.TryParse(input_line_1.Split(' ')[0] , out a)) return;
+            if(a < 200000 || a > 1000000) return;
+
+            int b;
+            if(!int.TryParse(input_line_1.Split(' ')[1] , out b)) return;
+            if(b < 1 || b > 6) return;
 
 
-            for(int i=0; i<input_line_1.Length; i++)
-            {
-                Console.WriteLine(input_line_1[i]);
-            }
+            int result = a*b;
+
+            Console.WriteLine(result.ToString());
         }
     }
 }
