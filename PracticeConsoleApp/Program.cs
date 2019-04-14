891f2235ffa36012471bdb6dd806595955a776fc
ikeda3642 <makiikeda3642@gmail.com>
2019-04-14 21:17:23 +0900

    P_D099 : complete

--- PracticeConsoleApp/Program.cs
+++ PracticeConsoleApp/Program.cs
@@ -1,33 +1,19 @@
 ﻿using System;
 
-namespace P_D100
+namespace P_D099
 {
-    
     class Program
     {
-        public static int CountChar(string s, char c) {
-            return s.Length - s.Replace(c.ToString(), "").Length;
-        }
         static void Main(string[] args)
         {
             //Console.WriteLine("please input S");
             string input_line_1 = Console.ReadLine();
 
-            int characterCount_1 = CountChar(input_line_1,'_');
-            int characterCount_2 = CountChar(input_line_1,'-');
 
-            string result = "";
-
-            if((characterCount_1==characterCount_2)|| (characterCount_1>characterCount_2))
+            for(int i=0; i<input_line_1.Length; i++)
             {
-                result = input_line_1.Replace("-","_");
+                Console.WriteLine(input_line_1[i]);
             }
-            else
-            {
-                result = input_line_1.Replace("_","-");
-            }
-
-            Console.WriteLine(result);
         }
     }
 }
