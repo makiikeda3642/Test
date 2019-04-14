c8c0b106bf9877a509bb3003c000213d4db4f3f6
ikeda3642 <makiikeda3642@gmail.com>
2019-04-14 20:32:20 +0900

    P_D106 : complete

--- PracticeConsoleApp/Program.cs
+++ PracticeConsoleApp/Program.cs
@@ -1,22 +1,27 @@
 ﻿using System;
 
-namespace P_D107
+namespace P_D106
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("please input S");
+            //Console.WriteLine("please input M");
             string input_line_1 = Console.ReadLine();
-            
-            string S = input_line_1;
-            
-            //Console.WriteLine("please input c");
+
+            int N;
+            if(!int.TryParse(input_line_1 , out N)) return;
+            if(N < 1 || N > 100) return;
+
+            //Console.WriteLine("please input N");
             string input_line_2 = Console.ReadLine();
+            int M;
+            if(!int.TryParse(input_line_2 , out M)) return;
+            if(M < 1 || M > 100) return;
 
-            string c = input_line_2;
+            int result = N%M;
 
-            Console.WriteLine(c + S + c);
+            Console.WriteLine(result.ToString());
         }
     }
 }
