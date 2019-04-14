04b64883742ddc68d1efa5756415dd0141bcee05
ikeda3642 <makiikeda3642@gmail.com>
2019-04-14 21:00:21 +0900

    P_D101 : Complete

--- PracticeConsoleApp/Program.cs
+++ PracticeConsoleApp/Program.cs
@@ -1,23 +1,35 @@
 ﻿using System;
 
-namespace P_D102
+namespace P_D101
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("please input N");
+            //Console.WriteLine("please input N M");
             string input_line_1 = Console.ReadLine();
 
             int N;
-            if(!int.TryParse(input_line_1 , out N)) return;
-            if(N < 1 || N > 30) return;
+            if(!int.TryParse(input_line_1.Split(' ')[0] , out N)) return;
+            if(N < 1 || N > 50) return;
 
-            int result = 100;
+            int M;
+            if(!int.TryParse(input_line_1.Split(' ')[1] , out M)) return;
+            if(M < 1 || M > 50) return;
 
-            result+=(N*10);
 
-            Console.WriteLine(result.ToString());
+            string result = "NO";
+
+            if(N%2==0 && M%2==1)
+            {
+                result = "YES";
+            }
+            else if(N%2==1 && M%2==0)
+            {
+                result = "YES";
+            }
+
+            Console.WriteLine(result);
         }
     }
 }
