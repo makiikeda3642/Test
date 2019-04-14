12c10ac91a8582d9c76b506818f71c93b366c7d3
ikeda3642 <makiikeda3642@gmail.com>
2019-04-14 22:49:53 +0900

    P_D092 : not comp score 90

--- PracticeConsoleApp/Program.cs
+++ PracticeConsoleApp/Program.cs
@@ -1,25 +1,58 @@
 ﻿using System;
 
-namespace P_D093
+namespace P_D092
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("please input N M");
+            //Console.WriteLine("please input");
             string input_line_1 = Console.ReadLine();
-            int N;
-            if(!int.TryParse(input_line_1 , out N)) return;
-            if(N < 1000 || N > 50000) return;
+            double x_1;
+            if(!double.TryParse(input_line_1.Split(' ')[0] , out x_1)) return;
+            if(x_1 < 1 || x_1 > 30) return;
 
-            string result = input_line_1;
+            double y_1;
+            if(!double.TryParse(input_line_1.Split(' ')[1] , out y_1)) return;
+            if(y_1 < 1 || y_1 > 30) return;
 
-            if(input_line_1.Replace(input_line_1[0].ToString(),"") != "")
+            double p_1;
+            if(!double.TryParse(input_line_1.Split(' ')[2] , out p_1)) return;
+            if(p_1 < 1000 || p_1 > 50000) return;
+
+            string input_line_2 = Console.ReadLine();
+            double x_2;
+            if(!double.TryParse(input_line_2.Split(' ')[0] , out x_2)) return;
+            if(x_2 < 1 || x_2 > 30) return;
+
+            double y_2;
+            if(!double.TryParse(input_line_2.Split(' ')[1] , out y_2)) return;
+            if(y_2 < 1 || y_2 > 30) return;
+
+            double p_2;
+            if(!double.TryParse(input_line_2.Split(' ')[2] , out p_2)) return;
+            if(p_2 < 1000 || p_2 > 50000) return;
+
+            double price_sheet1 = p_1/(x_1*y_1);
+            double price_sheet2 = p_2/(x_2*y_2);
+            
+            string result = "";
+            //Console.WriteLine(price_sheet1);
+            //Console.WriteLine(price_sheet2);
+            if(price_sheet1 == price_sheet2)
             {
-                result = "No";
+                result = "DRAW";
+            }
+            else if(price_sheet1 < price_sheet2)
+            {
+                result = input_line_1;
+            }
+            else
+            {
+                result = input_line_2;
             }
 
-            Console.WriteLine(result.ToString());
+            Console.WriteLine(result);
         }
     }
 }
