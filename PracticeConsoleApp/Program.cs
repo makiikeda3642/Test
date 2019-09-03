35e3593f1a744fbdad670202da3a95ea7e078f80
ikeda3642 <makiikeda3642@gmail.com>
2019-09-04 04:56:44 +0900

    P_D091 : score 100

--- PracticeConsoleApp/Program.cs
+++ PracticeConsoleApp/Program.cs
@@ -1,26 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace P_D096
+namespace P_D091
 {
     class Program
     {
-        public static int CountChar(string s, char c) {
-            return s.Length - s.Replace(c.ToString(), "").Length;
-        }
+        const int P_I_MAX = 10;
+        const int P_I_MIN = 1;
+        const int P_NUM_MAX = 5;
+        const int P_NUM_MIN = 1;
+        const int ALLOW_OUT_LEVEL = 2;
+        
         static void Main(string[] args)
         {
+            int result = 0;
+
             //Console.WriteLine("please input");
-            string input_line_1 = Console.ReadLine();
 
-            int have_I = CountChar(input_line_1,'I') > 0 ? 1:0;
-            int have_l = CountChar(input_line_1,'l') > 0 ? 1:0;
-            int have_i = CountChar(input_line_1,'i') > 0 ? 1:0;
+            var input_data_lsit = Console.ReadLine().Split(' ');
+            var p_i = input_data_lsit.Length;
 
-            string result = input_line_1;
+            if(!(p_i <= P_I_MAX && p_i >= P_I_MIN)) return;
 
-            if((have_I + have_l + have_i) >= 2)
+            List<int> p_list = new List<int>();
+            for(int i=0;i<p_i;i++)
             {
-                result = "caution";
+                int p;
+                
+                if(!int.TryParse(input_data_lsit[i] , out p)) return;
+                if(!(p>=P_NUM_MIN && p <= P_NUM_MAX))  return;
+
+                p_list.Add(p);
+            }
+
+            foreach(var p in p_list)
+            {
+                if(p <= ALLOW_OUT_LEVEL)result++;
             }
 
             Console.WriteLine(result);
